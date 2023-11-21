8b4d2905d3afaa7f91afd39a011309be55a75dee
jdagobethm <149737359+jdagobethm@users.noreply.github.com>
2023-11-21 22:31:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var skillsMember01 = document.getElementById("skillsMember1");
+    var skillsMember02 = document.getElementById("skillsMember2");
+    var skillsMember03 = document.getElementById("skillsMember3");
+    var skillsMember04 = document.getElementById("skillsMember4");
+    var skillsMember05 = document.getElementById("skillsMember5");
+    var skillsMember06 = document.getElementById("skillsMember6");
+    var skillsMember07 = document.getElementById("skillsMember7");
+    var skillsMember08 = document.getElementById("skillsMember8");
+    var skillsMember09 = document.getElementById("skillsMember9");
+    var skillsMember10 = document.getElementById("skillsMember10");
+}
